4afb99121d73463dc0254f1da445c45146f7ad5b
mhall903 <48227411+mhall903@users.noreply.github.com>
2019-06-16 19:08:27 -0700

    added model functionality and db

--- src/Model.java
+++ src/Model.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,14 +25,40 @@ public class Model {
     }
 
     public boolean verifyCredentials(String _username, String _password) {
-        return true;
+
+        return UserDatabase.getInstance().verifyCreds(_username,_password);
     }
 
     public boolean registerUser(String username, String email, String password) {
+        if(UserDatabase.getInstance().verifyCreds(username,password)){
+            return false;
+        }
+        UserDatabase.getInstance().addUsertobase(UserDatabase.getInstance().createUser(username,email,password,false));
         return true;
     }
 
-    public String recoverPassword(String email) {
-        return "password";
+    public String recoverPassword(String email) throws Exception{
+        try{
+            return UserDatabase.getInstance().getPassword(email);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        throw new Exception("Error Message: No email and Password found");
+    }
+    public void deleteAccount(String username, String password) throws Exception{
+        try{
+            UserDatabase.getInstance().removeUser(username,password);
+        }catch(Exception e){}
+    }
+    public void changePassword(String username, String newPassword) throws Exception{
+        try{
+            UserDatabase.getInstance().changePassword(username,newPassword);
+        }catch(Exception e){
+
+        }
+    }
+    public void save() throws IOException{
+        UserDatabase.getInstance().save();
     }
 }
